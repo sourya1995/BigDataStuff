e3139c7455e556c9af14a81b834b0b85eafc9951
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-04-04 18:10:02 +0530

    adding contiguous events with simple condition

--- Flink/src/main/java/CEP/StreamingJob.java
+++ Flink/src/main/java/CEP/StreamingJob.java
@@ -5,6 +5,7 @@ import org.apache.flink.cep.PatternSelectFunction;
 import org.apache.flink.cep.PatternStream;
 import org.apache.flink.cep.functions.PatternProcessFunction;
 import org.apache.flink.cep.pattern.Pattern;
+import org.apache.flink.cep.pattern.conditions.SimpleCondition;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.util.Collector;
@@ -16,15 +17,30 @@ public class StreamingJob {
     public static void main(String[] args) throws Exception {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         DataStream<String> input = env.fromElements(
-                "Alice", "Alice", "Bob", "Carly", "Carly", "Desmond", "Elise", "Carly", "Elise"
+                "Fred", "Ginger", "Bob", "Fred", "Ginger", "Ginger", "Fred", "Carly", "Ginger"
         );
-        Pattern<String, ?> pattern = Pattern.begin("start");
+        Pattern<String, ?> pattern = Pattern.<String>begin("first").where(new SimpleCondition<String>() {
+            @Override
+            public boolean filter(String value) throws Exception {
+                return value.equals("Fred");
+            }
+        }).next("second").where(new SimpleCondition<String>() {
+            @Override
+            public boolean filter(String value) throws Exception {
+                return value.equals("Ginger");
+            }
+        }).next("third").where(new SimpleCondition<String>() {
+            @Override
+            public boolean filter(String value) throws Exception {
+                return value.equals("Bob");
+            }
+        });
         PatternStream<String> patternStream = CEP.pattern(input, pattern);
         DataStream<String> matches = patternStream.select(new PatternProcessFunction<String, String>() {
 
             @Override
             public void processMatch(Map<String, List<String>> map, Context context, Collector<String> collector) throws Exception {
-                collector.collect(map.get("start").toString());
+                collector.collect(map.get("first").toString() + " -> " + map.get("second").toString() + map.get("third").toString() );
             }
         });
 

dcf601882755ae64a71c3663c8fbf00b15c19caa
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-04-04 19:30:42 +0530

    adding iterative conditions

--- Flink/src/main/java/CEP/StreamingJob.java
+++ Flink/src/main/java/CEP/StreamingJob.java
@@ -7,6 +7,7 @@ import org.apache.flink.cep.PatternSelectFunction;
 import org.apache.flink.cep.PatternStream;
 import org.apache.flink.cep.functions.PatternProcessFunction;
 import org.apache.flink.cep.pattern.Pattern;
+import org.apache.flink.cep.pattern.conditions.IterativeCondition;
 import org.apache.flink.cep.pattern.conditions.SimpleCondition;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -20,19 +21,30 @@ public class StreamingJob {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
 
-        DataStream<StockRecord> input = env.readTextFile("/path/to/file", 9999)
+        DataStream<StockRecord> input = env.readTextFile("/path/to/file")
                 .map(new ConvertToStockRecordFn());
-        Pattern<StockRecord, ?> pattern = Pattern.<StockRecord>begin("start").subtype(CloudPlatformStockRecord.class)
-                .next("middle").where(
-                        new SimpleCondition<StockRecord>() {
-                            @Override
-                            public boolean filter(StockRecord stockRecord) throws Exception {
-                                return stockRecord.getTags().contains("something");
-                            }
-                        }
-                );
+        Pattern<StockRecord, ?> pattern = Pattern.<StockRecord>begin("start")
+                .where(new SimpleCondition<StockRecord>() {
+                    @Override
+                    public boolean filter(StockRecord stockRecord) throws Exception {
+                        return false;
+                    }
+                })
+                .next("middle").where(new IterativeCondition<StockRecord>() {
+                    @Override
+                    public boolean filter(StockRecord stockRecord, Context<StockRecord> context) throws Exception {
+                        double sum = 0;
+                        int count = 0;
 
-        PatternStream<StockRecord> patternStream = CEP.pattern(event, pattern);
+                        for(StockRecord record: context.getEventsForPattern("start")) {
+                            sum += record.getClosingPrice();
+                            count++;
+                        }
+
+                        return (sum/count) > 1440;
+                    }
+                });
+        PatternStream<StockRecord> patternStream = CEP.pattern(input, pattern);
         DataStream<StockRecord> matches = patternStream.select(new PatternProcessFunction<StockRecord, String>() {
 
             @Override

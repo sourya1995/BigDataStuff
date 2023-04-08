3c79ba153be442b5bdb1d75a89ae7a9d20f8187b
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-04-08 08:38:21 +0530

    adding beam

---
+++ Flink/src/main/java/Beam/Filtering.java
@@ -0,0 +1,52 @@
+package Beam;
+
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Filtering {
+    public static class FilterThresholdFn extends DoFn<Double, Double> {
+        private double threshold = 0;
+
+        public FilterThresholdFn(double threshold) {
+            this.threshold = threshold;
+        }
+
+        @ProcessElement
+        public void processElement(ProcessContext c){
+            if(c.element() > threshold){
+                c.output(c.element());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        PipelineOptions options = PipelineOptionsFactory.create();
+        Pipeline pipeline = Pipeline.create(options);
+
+        List<Double> googStockPrices = Arrays.asList(1367.56, 1360.66, 1394.20, 1393.33, 1401.31, 1419.82, 1429.73);
+
+        pipeline.apply(Create.of(googStockPrices))
+                .apply(MapElements.via(new SimpleFunction<Double, Double>() {
+                    @Override
+                    public Double apply(Double input){
+                        System.out.println("Pre-filtered" + input);
+                        return input;
+                    }
+                }))
+                        .apply(ParDo.of(new FilterThresholdFn(1400)))
+                                .apply(MapElements.via(new SimpleFunction<Double, Void>() {
+                                    @Override
+                                    public Void apply(Double input){
+                                        System.out.println("Post-filtered" + input);
+                                        return null;
+                                    }
+                                }));
+
+        pipeline.run();
+    }
+}
---
+++ Flink/src/main/java/Beam/PipelineRunner.java
@@ -0,0 +1,10 @@
+package Beam;
+
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+
+public class PipelineRunner {
+    public static void main(String[] args) {
+        PipelineOptions options = PipelineOptionsFactory.create();
+    }
+}
